d40d7e838862fdf39e351a8b344804df2ec02a71
Saho Yoshihara <sahoyoshihara@icloud.com>
2020-09-20 20:28:44 +0900

    ticket speakers inf changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -159,7 +159,7 @@ const Blog = ({ children }) => {
         <div className="container mx-auto px-0">
             <div className="pt-20 pb-10 text-center lg:pt-40 lg:pb-20">
                 <h2 className="text-color-1 font-black text-5xl lg:text-6xl">
-                    Blog
+                    Speakers
                 </h2>
             </div>
             <div className="flex flex-wrap">{children}</div>
